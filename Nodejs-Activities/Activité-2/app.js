4f757b1e10c3ce218731c02ee6ebabf0b75e4980
abdelaaziz khouda <abdelkhouda055@gmail.com>
2025-10-21 11:06:56 +0100

    node js activitises

---
+++ Nodejs-Activities/Activité-2/app.js
@@ -0,0 +1,5 @@
+const saluer = require("./utilities/greet");
+console.log(saluer("youness"));
+
+// le module exporte une seule fonction ou valeur.
+// le module exporte un objet contenant plusieurs fonctions ou valeurs
---
+++ Nodejs-Activities/Activité-4 /app.js
@@ -0,0 +1,20 @@
+const { ajouterContact, listerContact } = require("./contactService");
+const formaterContact = require('./utils/format');
+
+
+ajouterContact("Alice", "06060606")
+ajouterContact("Bob", "06011106")
+
+listerContact().forEach(c => console.log(formaterContact(c)))
+
+
+
+// Chaque module doit faire une seule tâche claire (ex : logique, affichage, ou configuration).
+
+// Pourquoi séparer formatage, logique et point d’entrée :
+// Formatage → gère l’affichage.
+// Logique → gère les calculs ou règles.
+// Point d’entrée → lance l’application.
+
+// Avantage pour la maintenance :
+// Le code est plus clair, réutilisable, facile à tester et à modifier sans casser le reste du projet.
